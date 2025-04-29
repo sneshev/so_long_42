f9dab81f1f588ce8f5d777e0ce8ce8cc1064dffb
John Doe <steffal.n2@gmail.com>
2025-04-29 21:06:45 +0200

    draws empty widnow, images ready to be drawn

--- src/so_long.c
+++ src/so_long.c
@@ -1,16 +1,52 @@
 #include "so_long.h"
 
+mlx_image_t *get_image(int type, mlx_t *mlx)
+{
+	mlx_texture_t* wall_tex;
+	mlx_texture_t* floor_tex;
+	mlx_texture_t* player_tex;
+	mlx_texture_t* exit_tex;
+	mlx_texture_t* collect_tex;
 
+	wall_tex = mlx_load_png("assets/wall.png");
+	floor_tex = mlx_load_png("assets/floor.png");
+	player_tex = mlx_load_png("assets/player.png");
+	exit_tex = mlx_load_png("assets/exit.png");
+	collect_tex = mlx_load_png("assets/collectible.png");
+	if (type == WALL)
+		return (mlx_texture_to_image(mlx, wall_tex));
+	if (type == PLAYER)
+		return (mlx_texture_to_image(mlx, player_tex));
+	if (type == COLLECTIBLE)
+		return (mlx_texture_to_image(mlx, collect_tex));
+	if (type == EXIT)
+		return (mlx_texture_to_image(mlx, exit_tex));
+	if (type == EMPTY)
+		return (mlx_texture_to_image(mlx, floor_tex));
+	return (NULL);		
+}
 
-// void start_game(char *map)
-// {
-// 	mlx_t *mlx;
-	// if (!(mlx = mlx_init(find("width", map), find("height", map), "idk", false)))
-// 	{
-// 		// puts();
-// 		return (1);
-// 	}
-// }
+void start_game(char *map)
+{
+	mlx_t *mlx;
+	int width;
+	int height;
+
+	width = find(WIDTH, map) * TILE_SIZE;
+	height = find(HEIGHT, map) * TILE_SIZE;
+	if (!(mlx = mlx_init(width, height, "i love mitaniiiiiiiiiiiiiiiii", false)))
+	{
+		// puts();
+		return ;
+	}
+
+    // Start the rendering loop
+    mlx_loop(mlx);
+
+    // Clean up when the window closes
+    mlx_terminate(mlx);
+    return ;
+}
 
 
 int main(int argc, char *argv[])
@@ -32,7 +68,7 @@ int main(int argc, char *argv[])
 	
 	printf("mapp good :)");
 	
-	// start_game(map);
+	start_game(map);
 
 	free(map);
 }
