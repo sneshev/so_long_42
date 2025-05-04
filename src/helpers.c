bc1d581ff808e54578e810f30c3d041f48996bca
sneshev <steffal.n2@gmail.com>
2025-05-04 09:41:40 +0200

    commented printf()s

--- src/helpers.c
+++ src/helpers.c
@@ -1,23 +1,23 @@
 #include "so_long.h"
 
-void print_map(char *map)
-{
-	printf("⬇️	map[]	⬇️\n");
-	printf("width: %d, height: %d\n\n", find(WIDTH, map), find(HEIGHT, map));
-	printf("%s", map);
-	printf("\n\n");
-}
+// void print_map(char *map)
+// {
+// 	printf("⬇️	map[]	⬇️\n");
+// 	printf("width: %d, height: %d\n\n", find(WIDTH, map), find(HEIGHT, map));
+// 	printf("%s", map);
+// 	printf("\n\n");
+// }
 
-void print_map2(char **map)
-{
-	int i;
+// void print_map2(char **map)
+// {
+// 	int i;
 
-	i = 0;
-	printf("⬇️	map[]	⬇️\n\n");
-	while (map[i])
-	{
-		printf("%s\n", map[i]);
-		i++;
-	}
-	printf("\n\n");
-}
+// 	i = 0;
+// 	printf("⬇️	map[]	⬇️\n\n");
+// 	while (map[i])
+// 	{
+// 		printf("%s\n", map[i]);
+// 		i++;
+// 	}
+// 	printf("\n\n");
+// }
